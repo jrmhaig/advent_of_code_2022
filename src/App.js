c12ec950a54e836a5e61976cf908665a0bc35e5e
Joe Haig <joseph.haig@digital.justice.gov.uk>
2022-11-07 10:43:32 +0000

    Add bulma

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className="section">
+      <div className="columns">
+        <div className="column is-3">
+          <p>Sidebar</p>
+        </div>
+        <div className="column is-9">
+          <section className="main">
+            <p>Main stuff</p>
+          </section>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default App;

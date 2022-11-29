764164ce21fe227944f30dc91c74d48711bc3816
Joe Haig <joseph.haig@digital.justice.gov.uk>
2022-11-29 09:15:02 +0000

    Add 2021 day one part two solution

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,12 @@ const Home = () => {
   return (
     <div className="container">
       <p>
-        These pages are my attempt to solve the <a href="https://adventofcode.com/">2022 Advent of Code</a>
-        while learning <a href="https://reactjs.org/">React</a>
-        and <a href="https://bulma.io/">Bulma.</a>
-        This being the case, things are liable to change and (probably) break.
-        Also, there is no guarantee that I will get to day 25.
+        These pages are my attempt to solve the <a href="https://adventofcode.com/">2022 Advent of Code</a> while
+        learning <a href="https://reactjs.org/">React</a> and <a href="https://bulma.io/">Bulma.</a> This being the
+        case, things are liable to change and (probably) break. Also, there is no guarantee that I will get to day 25.
       </p>
+
+      <p>The source code for this is on <a href="https://github.com/jrmhaig/advent_of_code_2022">Github</a></p>
     </div>
   )
 }
--- src/components/TestDay.js
+++ src/components/TestDay.js
@@ -4,17 +4,19 @@ const TestDay = () => {
   const [inputData, setInputData] = useState('');
   const [partOneSolution, setPartOneSolution] = useState(null);
   const [partOneWorking, setPartOneWorking] = useState(null);
+  const [partTwoSolution, setPartTwoSolution] = useState(null);
+  const [partTwoWorking, setPartTwoWorking] = useState(null);
 
   const partOne = () => {
-    const depths = inputData.split(/\r?\n/);
+    const depths = inputData.split(/\r?\n/).map(n => +n);
     const length = depths.length;
     let newSolution = [`${depths[0]} (N/A - no previous measurement)`];
     let tally = 0;
     for (let i = 1; i < length; i++) {
-      if (+depths[i] > +depths[i - 1]) {
+      if (depths[i] > depths[i - 1]) {
         tally += 1;
         newSolution.push(`${depths[i]} (increased) - ${tally}`);
-      } else if (+depths[i] < +depths[i - 1]) {
+      } else if (depths[i] < depths[i - 1]) {
         newSolution.push(`${depths[i]} (decreased)`);
       } else {
         newSolution.push(`${depths[i]} (no change)`);
@@ -22,6 +24,27 @@ const TestDay = () => {
     }
     setPartOneSolution(tally);
     setPartOneWorking(newSolution);
+  }
+
+  const partTwo = () => {
+    const depths = inputData.split(/\r?\n/).map(n => +n);
+    const length = depths.length;
+    let newSolution = [`${depths[2] + depths[1] + depths[0]} (N/A - no previous measurement)`];
+    let tally = 0;
+    for (let i = 3; i < length; i++) {
+      const a = depths[i] + depths[i - 1] + depths[i - 2];
+      const b = depths[i - 1] + depths[i - 2] + depths[i - 3];
+      if (a > b) {
+        tally += 1;
+        newSolution.push(`${a} (increased) - ${tally}`);
+      } else if (a < b) {
+        newSolution.push(`${a} (decreased)`);
+      } else {
+        newSolution.push(`${a} (no change)`);
+      }
+    }
+    setPartTwoSolution(tally);
+    setPartTwoWorking(newSolution);
   }
 
   return (
@@ -46,9 +69,12 @@ const TestDay = () => {
 
       <div className="field">
         <div className="control">
-          <button className="button">Solve part 2</button>
+          <button className="button" onClick={partTwo}>Solve part 2</button>
         </div>
       </div>
+
+      {partTwoSolution && <p>Solution: {partTwoSolution}</p>}
+      {partTwoWorking && <div className="box">{partTwoWorking.map(row => <p>{row}</p>)}</div>}
     </div >
   );
 }
